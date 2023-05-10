52696a777ad2ba3924fd3b182e61835b2279a2db
la-sarita <sarirosa@saras-mbp>
2023-05-10 07:41:32 -0400

    Initial Commit

---
+++ src/bonjournom/BonjourNom.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bonjournom;
+
+/**
+ *
+ * @author sarirosa
+ */
+public class BonjourNom {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.printf("Bonjour %s %s \n", args[0], args[1]);
+    }
+    
+}
